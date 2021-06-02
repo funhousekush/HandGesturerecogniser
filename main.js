454847e47178eb55d9bee8c01078d48ed8a39081
funhousekush <85101915+funhousekush@users.noreply.github.com>
2021-06-02 20:09:17 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ Webcam.attach("#camera");
 function take_snapshot()
 {
     Webcam.snap(function(data_uri){
-        document.getElementById("result").innerHTML = '<img id="capture_image" src="'+data_uri+'"/>';
+        document.getElementById("snapshot").innerHTML = '<img id="capture_image" src="'+data_uri+'"/>';
     });
 }
 
